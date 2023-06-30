4497242cb04fc00fffbc2f673b32cd088e15c5cf
LIOU2021 <aa78789898tw@gmail.com>
2023-07-01 00:31:15 +0800

    create init sql

---
+++ init.sql
@@ -0,0 +1,13 @@
+create database wallet;
+use wallet;
+CREATE TABLE accounts (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) UNIQUE,
+  balance DECIMAL(10, 2)
+);
+
+INSERT INTO accounts (username, balance) VALUES
+('A', 1000.00),
+('B', 500.00),
+('C', 200.00),
+('D', 300.00);
